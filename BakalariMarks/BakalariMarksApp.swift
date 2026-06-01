2346ff03fbb573aa143ee6f375859ef407f88fe2
Filip Bukovina <filip@bukovinafilip.com>
2026-06-01 17:27:09 +0200

    Initial Commit

---
+++ BakalariMarks/BakalariMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakalariMarksApp.swift
+//  BakalariMarks
+//
+//  Created by Filip Bukovina on 01.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BakalariMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakalariMarksTests/BakalariMarksTests.swift
@@ -0,0 +1,19 @@
+//
+//  BakalariMarksTests.swift
+//  BakalariMarksTests
+//
+//  Created by Filip Bukovina on 01.06.2026.
+//
+
+import Testing
+@testable import BakalariMarks
+
+struct BakalariMarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

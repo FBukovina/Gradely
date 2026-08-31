d45e9c43bbbde079a00f7b4fe79756cdafb34bbe
Filip Bukovina <filip@bukovinafilip.com>
2026-08-31 21:37:28 +0200

    chore(android): set first beta version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.1.0-beta.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "SUPABASE_URL", "\"${providers.gradleProperty("gradey.supabaseUrl").orElse("").get()}\"")
